44e0695e88dbe13fc7699022ec977d81f0833374
Andrea Venturini <andrea.venturini@bancaditalia.it>
2022-09-20 06:40:02 +0000

    version 0.1.3

--- R/dati.R
+++ R/dati.R
@@ -1,15 +1,14 @@
-#' Data frame of meteorological data
+#' @title Data frame of meteorological data
 #'
-#' A dataset containing values to test outlier detection on meteorological number completely random
+#' @description This sample data would be invented meteorological information detected by weather stations.
 #'
 #'
 #' @format A data frame with 800 rows and 4 variables:
 #' \describe{
 #'   \item{phen}{Temperature, Rain}
-#'   \item{time}{ordered numbers for time}
-#'   \item{zone}{label classification for group data}
-#'   \item{value}{numbers for values}
+#'   \item{time}{ordered numbers for time (a number in the format YYYYMMDD [Year Month Day] is possible too)  }
+#'   \item{zone}{label classification for the group, for example the identification code of a wheather station.}
+#'   \item{value}{values}
 #'
 #' }
 "dati"
-
--- R/ts.R
+++ R/ts.R
@@ -1,6 +1,6 @@
-#' Time series
+#' @title Time series
 #'
-#' A dataset containing a random time series with increasing trend and some variability
+#' @description This is an example of a single time series with increasing trend and some variability.
 #'
 #' @format A data frame with 35 rows and 1 variable:
 #' \describe{
